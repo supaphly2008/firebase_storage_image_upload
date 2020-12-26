5e0d47f13735d05f7acaf7d9a349bf6e41188a74
Terry Liao <terry.liao@nogle.com>
2020-12-26 22:43:30 +0800

    add delete function

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import Swal from "sweetalert2";
+import { firebaseApp } from "../firebase/firebase";
 import useFirestore from "../hooks/useFirestore";
 
 const ImageContainer = styled.div`
@@ -10,6 +12,7 @@ const ImageContainer = styled.div`
 `;
 
 const Img = styled.img`
+  border-radius: 5px;
   width: 200px;
   height: 200px;
   object-fit: cover;
@@ -19,17 +22,44 @@ const Img = styled.img`
 
   @media screen and (max-width: 480px) {
     width: 100%;
-    height: auto;
+    height: 200px;
   }
 `;
 
 const ImageGrid = () => {
   const { docs } = useFirestore("images");
 
+  const onImageDelete = (doc) => {
+    const desertRef = firebaseApp.storage().refFromURL(doc.url);
+
+    firebaseApp
+      .firestore()
+      .collection("images")
+      .doc(doc.id)
+      .delete()
+      .then(() => {
+        desertRef
+          .delete()
+          .then(() => {
+            Swal.fire({
+              icon: "success",
+              title: "Successful",
+              text: "Your Image has been deleted!",
+            });
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <ImageContainer>
       {docs.map((doc) => (
-        <Img key={doc.id} src={doc.url} />
+        <Img key={doc.id} src={doc.url} onDoubleClick={() => onImageDelete({ id: doc.id, url: doc.url })} />
       ))}
     </ImageContainer>
   );
